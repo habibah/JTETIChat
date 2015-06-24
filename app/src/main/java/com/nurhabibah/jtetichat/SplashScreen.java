eff28888d22c5fbad0b3e7844f8a5abb7e8747b6
arumpuspa <aruma@outlook.co.id>
2015-06-24 20:56:21 +0700

    tambah splash screen

---
+++ app/src/main/java/com/nurhabibah/jtetichat/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.nurhabibah.jtetichat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Aruma on 24-Jun-15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent("com.nurhabibah.jtetichat.MAINACTIVITY");
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
